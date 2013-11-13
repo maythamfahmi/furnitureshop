48d5d4ecfb32019abe74fbefa2866ef4b830dd95
jakobvinther <jakob@vinther-larsen.dk>
2013-11-13 22:47:07 +0100

    Minor change in subcategory (virtual "not-null")

--- FurnitureShop/Models/SubCategory.cs
+++ FurnitureShop/Models/SubCategory.cs
@@ -18,6 +18,6 @@ namespace FurnitureShop.Models
 		public string Name { get; set; }
 
 		public int? CategoryId { get; set; } //this is nullable
-		public virtual Category? Category { get; set; }
+		public virtual Category Category { get; set; }
 	}
 }
