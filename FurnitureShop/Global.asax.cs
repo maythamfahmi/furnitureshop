873af3807fae146f0a0a9910938c7cef6ccbb80d
Maytham Fahmi <maythamfahmi@itbackyard.com>
2018-01-21 23:33:23 +0100

    few updates, (db string to be fixed)

--- FurnitureShop/Global.asax.cs
+++ FurnitureShop/Global.asax.cs
@@ -12,22 +12,22 @@ using System.Web.Security;
 
 namespace FurnitureShop
 {
-	internal class MyNinjectModule : NinjectModule
-	{
-		public override void Load()
-		{
-			Bind<IUserRepository>().To<UserRepository>();
-			//Bind<IAccountRoleRepository>().To<EFAccountRoleRepository>();
-		}
-	}
+    internal class MyNinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IUserRepository>().To<UserRepository>();
+            //Bind<IAccountRoleRepository>().To<EFAccountRoleRepository>();
+        }
+    }
     public class MvcApplication : System.Web.HttpApplication
     {
-		// Enable DI of membership provider
-		private readonly IKernel _ninjectKernel = new StandardKernel(new MyNinjectModule());
+        // Enable DI of membership provider
+        private readonly IKernel _ninjectKernel = new StandardKernel(new MyNinjectModule());
         protected void Application_Start()
         {
             //System.Data.Entity.Database.SetInitializer(new System.Data.Entity.DropCreateDatabaseIfModelChanges<FurnitureShop.Models.FurnitureShopContext>());
-            
+
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
@@ -39,8 +39,8 @@ namespace FurnitureShop
 
             ModelBinders.Binders.Add(typeof(Cart), new CartModelBinder());
 
-			_ninjectKernel.Inject(Membership.Provider);
-			_ninjectKernel.Inject(Roles.Provider);
+            _ninjectKernel.Inject(Membership.Provider);
+            _ninjectKernel.Inject(Roles.Provider);
         }
     }
 }
